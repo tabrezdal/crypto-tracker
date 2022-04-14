960618ac3287c2b2b47dccac2428e8c4352e99a5
Tabrez Dal <tabrez.dal@gmail.com>
2022-04-14 06:58:22 +0530

    credits added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import './App.css';
 import axios from 'axios';
 import Coin from './comps/Coin';
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCM44G81Hdmt6NXyOtoTDQ_XvGQzAUZJP0",
+  authDomain: "crypto-tracker-5a3c1.firebaseapp.com",
+  projectId: "crypto-tracker-5a3c1",
+  storageBucket: "crypto-tracker-5a3c1.appspot.com",
+  messagingSenderId: "511670475099",
+  appId: "1:511670475099:web:85987d3888d9c95ceda27e",
+  measurementId: "G-2LLWNG5P49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
@@ -30,12 +56,13 @@ function App() {
     <div className="coin-app">
     <div className="coin-search">
       <h1 className="coin-text">
-          Search a Cyrpto Currency
+          TEZ Crypto Tracker
       </h1>
       <form>
          <input className='coin-input' type="text" placeholder="search here..." onChange={ handelChange }/>
      </form>
   </div>
+  <div className='coins-container'>
   
   {coins.map(coin => {
     return (
@@ -51,6 +78,7 @@ function App() {
     );
     })}
       
+    </div>
     </div>
   );
 }
---
+++ src/comps/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const footer = () => {
+  return (
+    <div className='footer'>
+        <div className='credit-name'>
+            <p>Designed By Tabrez Dal</p>
+        </div>
+        <div className='credit-link'>
+            <a href='https://www.tzcreation.in'>TZ Creation</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default footer
